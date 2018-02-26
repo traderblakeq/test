ce52a924a3bb84210006e35aec0553fa5f0eaaf6
Mac OS X <macosx@Trader-Blake.local>
2018-02-26 10:41:59 +0100

    Initialize commit

---
+++ Basket.java
@@ -0,0 +1,19 @@
+public class Basket{
+
+   private int productbasket[];
+   
+   public void addToBasket(){
+   }
+   
+   public void editItem(){
+   }
+   
+   public void removeItem(){
+   }
+   
+   public void calculateTotalPrice(){
+   }
+   
+   public void checkout(){
+   }
+}
